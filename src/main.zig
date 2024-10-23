1384d46899f79cb72547d312679777316622c5fd
OsakiTsukiko <osakitsukiko.mail@gmail.com>
2024-10-23 16:43:53 +0300

    add cmd arguments

--- src/main.zig
+++ src/main.zig
@@ -14,13 +14,27 @@ pub fn main() !void {
     }){};
     const allocator = gpa.allocator();
 
+    var args = std.process.args(); // command line arguments
+    
+    var port: usize = 3000; // default port 3000
+    
+    if (args.skip()) {
+        const port_string = args.next(); // get port argument
+        if (port_string) |port_string_nn| { // ignore null
+            if (fmt.parseInt(usize, port_string_nn, 10)) |prs_res| { // parse arg as int
+                port = prs_res;
+            } else |_| {}
+        }
+    }
+    
+
     state.init(allocator);
     defer state.deinit();
 
     std.debug.print("PK: {s}\n", .{fmt.bytesToHex(state.conf.public_key, .upper)});
     std.debug.print("SK: {s}\n", .{fmt.bytesToHex(state.conf.secret_key, .upper)});
 
-    var l = webListener.init(allocator, 3000);
+    var l = webListener.init(allocator, port);
     defer l.deinit();
 
     l.config();
